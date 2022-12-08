9a66806d827a33b485abab74f2d9768ba5e31f94
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-08 08:09:43 +0300

    Update and rename ecommerce/packetCagir.py to method_kullanım.py

--- ecommerce/packetCagir.py
+++
@@ -1,2 +0,0 @@
-def FiyatHesapla():
-    print("FiyatHesaplaniyor...")
---
+++ method_kullanım.py
@@ -0,0 +1,8 @@
+def FiyatHesapla(): #this is a method
+    print("FiyatHesaplaniyor...")
+    
+   
+# here we call the method
+
+
+FiyatHesapla()
